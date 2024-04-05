ccbcab48d6298d5de0579451235a735911e40779
KofE <bhiushi6564@gmail.com>
2024-04-05 15:42:00 +0500

    Add a books to main container and style them

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,57 @@
 const myLibrary = [];
 
-function Book(author, title, sales, read_status) {
-    this.author = author;
+function Book(title, author, pages, read_status) {
     this.title = title;
-    this.sales = sales;
-    this.readed_status = read_status;
+    this.author = author;
+    this.pages = pages;
+    this.read_status = read_status;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
 
-let book1 = new Book("J.K. Rowling", "Harry Potter and the Philosopher's Stone", ">500 million", true);
-let book2 = new Book("George Orwell", "1984", ">50 million", false);
-let book3 = new Book("J.R.R. Tolkien", "The Lord of the Rings", ">150 million", true);
-let book4 = new Book("Agatha Christie", "And Then There Were None", ">100 million", false);
-let book5 = new Book("Dan Brown", "The Da Vinci Code", ">80 million", false);
+let book1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "320");
+let book2 = new Book("1984", "George Orwell", "328");
+let book3 = new Book("The Lord of the Rings", "J.R.R. Tolkien", "1178");
+let book4 = new Book("And Then There Were None", "Agatha Christie", "272");
+let book5 = new Book("The Da Vinci Code", "Dan Brown", "489");
 
-
-myLibrary.push(book1,book2,book3,book4,book5)
+myLibrary.push(book1, book2, book3, book4, book5)
 console.log(myLibrary)
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+const container = document.querySelector(".main-container");
+myLibrary.forEach(add_books_to_library)
+
+function add_books_to_library(value){
+    let item = document.createElement("div");
+    item.classList.toggle("book-item");
+
+    let title = document.createElement('div');
+    title.classList.add('book-info-item');
+    title.textContent = value.title;
+
+    let author = document.createElement('div');
+    author.classList.add('book-info-item');
+    author.textContent = value.author;
+
+    let pages = document.createElement('div');
+    pages.classList.add('book-info-item');
+    pages.textContent = `${value.pages} pages`;
+
+    item.append(title, author, pages);
+
+    container.append(item);
+}
